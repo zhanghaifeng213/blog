3947099b11bbf5ca0d1773c78014d7c70512de31
zhanghf <zhanghf_123@sina.com>
2019-01-01 20:28:21 +0800

    avatar upload

--- web/src/api/user.js
+++ web/src/api/user.js
@@ -31,68 +31,11 @@ export const logout = () => {
   })
 }
 
-
-export const getUserInfo = userName => {
-  const userdata = new FormData()
-  userdata.append('loginName', userName)
+export const upload = (data) => {
   return http({
-    url: http.adornUrl('sys/getUserMessage'),
-    data: userdata,
-    method: 'post'
-  })
-}
-
-export const addUser = data => {
-  return http({
-    url: http.adornUrl('sys/addUser'),
+    url: http.adornUrl('upload'),
     method: 'post',
     data: data
   })
 }
 
-export const getMessage = () => {
-  return axios.request({
-    url: 'message/init',
-    method: 'get'
-  })
-}
-
-export const getContentByMsgId = msg_id => {
-  return axios.request({
-    url: 'message/content',
-    method: 'get',
-    params: {
-      msg_id
-    }
-  })
-}
-
-export const hasRead = msg_id => {
-  return axios.request({
-    url: 'message/has_read',
-    method: 'post',
-    data: {
-      msg_id
-    }
-  })
-}
-
-export const removeReaded = msg_id => {
-  return axios.request({
-    url: 'message/remove_readed',
-    method: 'post',
-    data: {
-      msg_id
-    }
-  })
-}
-
-export const restoreTrash = msg_id => {
-  return axios.request({
-    url: 'message/restore',
-    method: 'post',
-    data: {
-      msg_id
-    }
-  })
-}
